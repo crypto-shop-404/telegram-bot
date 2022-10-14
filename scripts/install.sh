3c89b5d5aa768cdb3bf708e65f17a92b9afb5518
altcode <cosmosx1328@gmail.com>
2022-10-14 20:57:02 +0600

    :rocket: Rename supervisord to supervisor, move poetry to top

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 distributive=$( hostnamectl | grep -E 'Transient hostname\: .+' )
+curl -sSL https://install.python-poetry.org | python3 -
+poetry install
 if [ "$distributive" == 'Transient hostname: ubuntu' ]; then
 sudo apt-get install supervisor
 fi
-sudo systemctl start supervisord
-sudo systemctl enable supervisord
+sudo systemctl start supervisor
+sudo systemctl enable supervisor
 if [ -e /etc/supervisor/conf.d/shop_bot.conf ]; then
 sudo nano /etc/supervisor/conf.d/shop_bot.conf
 fi
 supervisorctl start shop_bot
-
-curl -sSL https://install.python-poetry.org | python3 -
-poetry install
