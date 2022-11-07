3799818325246452fbd528c0d33e150331318aed
altcode <cosmosx1328@gmail.com>
2022-11-07 23:52:04 +0600

    :recycle: Rename waiting_category_name to waiting_name

--- src/states/category_states.py
+++ src/states/category_states.py
@@ -2,7 +2,7 @@ from aiogram.dispatcher.filters import state
 
 
 class AddCategories(state.StatesGroup):
-    waiting_category_name = state.State()
+    waiting_name = state.State()
 
 
 class AddSubcategories(state.StatesGroup):
